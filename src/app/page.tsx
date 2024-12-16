63d1207184ffcb38d15aebfee36a4fe48cd9724f
harahsavan <harshsavani02@gmail.com>
2024-12-16 12:37:43 +0530

    fix error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Form from "@/components/form/Form";
 import Header from "@/components/layout/header/Header";
 
+export const runtime = "edge";
+
 export default function Home() {
   return (
     <div>
